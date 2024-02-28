d8e438f4c61f6ae52a1e264c8b299420517687a1
Ankit Meshram <89692662+ankme5@users.noreply.github.com>
2024-02-28 12:16:37 +0530

    Create start_server.sh

---
+++ scripts/start_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the directory where your Node.js application is located
+cd /var/www/my-nodejs-app
+
+# Install dependencies (if needed)
+# npm install
+
+# Start the Node.js server
+node app.js
