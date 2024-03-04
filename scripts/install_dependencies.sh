1bfb0dcfd5cb04c3661899e435e7b87e2e2e501f
Ankit Meshram <89692662+ankme5@users.noreply.github.com>
2024-03-04 18:55:52 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the directory where your Node.js application is located
-cd /var/www/my-nodejs-app
+# cd /var/www/my-nodejs-app
 
 # Install dependencies using npm
-npm install
+# npm install
