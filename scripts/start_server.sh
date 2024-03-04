dd0cbf65cc1fecd3880761c4cbc2eb5f886c8a9b
Ankit Meshram <89692662+ankme5@users.noreply.github.com>
2024-03-04 18:28:54 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,5 @@
 
 # Navigate to the directory where your Node.js application is located
 cd /var/www/my-nodejs-app
-docker run -d -p 8000:8000 jenkins-node-app
+docker build . -t "jenkins-node-app"
+docker run -d -t "test1" -p 8000:8000 jenkins-node-app
